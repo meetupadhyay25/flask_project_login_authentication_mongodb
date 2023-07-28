bdcc99b5768a6f02f0b322121e45af23cd7b2ba0
meetupadhyay25 <111427109+meetupadhyay25@users.noreply.github.com>
2023-07-28 17:11:59 +0530

    Update forms.py

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, InputRequired, Email, Length
+from wtforms.validators import DataRequired, InputRequired, Length
 
 
 class RegForm(FlaskForm):
     username = StringField('UserName', validators=[DataRequired(), Length(4, 20)])
-    email = StringField('email',  validators=[InputRequired(), Email(message='Invalid email'), Length(max=30)])
+    email = StringField('email',  validators=[InputRequired(), Length(max=30)])
     password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=20)])
     submit = SubmitField('Create Account')
 
